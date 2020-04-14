18324d8cd8b567339ecc1011041c54cb2dc6a798
MagicDoogies <37319450+MagicDoogies@users.noreply.github.com>
2020-04-14 11:38:14 -0400

    commented on codes 22-34

--- Week9-CodeLab1-GridGame/Assets/Scripts/GridItem.cs
+++ Week9-CodeLab1-GridGame/Assets/Scripts/GridItem.cs
@@ -19,18 +19,18 @@ public class GridItem : MonoBehaviour
         gridX = x;         //sets the length of x on the grid
         gridY = y;         //sets the length of y on the grid
 
-        name = "X: " + x + " Y: " + y;
+        name = "X: " + x + " Y: " + y;//it prints the co-ordinates of the originally selected square.
     }
 
-    void OnMouseDown()
+    void OnMouseDown() //custom fucntion for when the player clicks down on the mouse.
     {
-        if(GridManager.instance.selected == null){
-            GridManager.instance.selected = this;
-            transform.localScale = new Vector3(1, 1, 1);
-        } else {
-            GridManager.instance.Swap(this);
+        if(GridManager.instance.selected == null){ //if there is no Grid Manager script
+            GridManager.instance.selected = this;//it makes one and activates the 'Swap(GridItem) function under the GridManager script.
+            transform.localScale = new Vector3(1, 1, 1);//The selected cube scales by 1 in X,Y,Z
+        } else { //else if you already clicked on the cube.
+            GridManager.instance.Swap(this);//activates the function in the GridManager script that swaps the positions of the selected cubes.
         }
 
-        print(name);
+        print(name); //prints out the co-ordinates of whatever is in line 22.
     }
 }

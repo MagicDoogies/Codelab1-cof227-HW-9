f9d7a86c837835ad03828b6420e9dbdef25c30de
MagicDoogies <37319450+MagicDoogies@users.noreply.github.com>
2020-04-15 16:49:21 -0400

    Mostly fixed code that allows the game to recognize when the player has completed the puzzle.

--- Week9-CodeLab1-GridGame/Assets/Scripts/GridManager.cs
+++ Week9-CodeLab1-GridGame/Assets/Scripts/GridManager.cs
@@ -19,6 +19,9 @@ public class GridManager : MonoBehaviour
 
     public GridItem selected; //this is initializing which cube we are selecting so we can swap
 
+    public static int CorrectCubes; //this is how many cubes are correct. when it reaches 16 we're good
+
+
     void Awake() //this is the singleton for the game object this script is attached to
     {
         if(instance == null){ //if theres no instance of this script
@@ -32,6 +35,9 @@ public class GridManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        CorrectCubes = 0; // start off with CorrectCubes being 0
+        
         grid = new GameObject[width, height]; // creates a new grid as a GameObject 
 
         GameObject gridHolder = new GameObject("Grid Holder"); // lets you set what prefab is holding the grid
@@ -84,5 +90,38 @@ public class GridManager : MonoBehaviour
         // These are to reset the selection states after the swap is over.
         selected.transform.localScale = new Vector3(.75f, .75f, .75f); // resets the size of the cube back to smaller again.
         selected = null; // resets the selection back to null after the swap is over.
+
+
+        // CHECK the 2 grids
+        CorrectCubes = 0; // start off with CorrectCubes being 0
+
+        // for loop that checks each position of the grid to see if it's correct
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+
+
+                // NOTE at 4:42pm 4/15/2020 - The for loop above seems to be working, but this if statement below 
+                //needs fixing!
+                if (grid[x, y].GetComponent<MeshRenderer>().material == grid2[x, y].GetComponent<MeshRenderer>().material)
+                {
+                    CorrectCubes += 1;
+                    Debug.Log("CorrectCubes: " + CorrectCubes);
+
+                    if (CorrectCubes == 2)
+                    {
+                        print("YOU ARE SMART. WIN");
+                    }
+                }
+
+            }
+        }
+
+
+        
+
+
+
     }
 }
